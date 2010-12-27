fec010fb393a97f41838113a58638b71cb28c4c4
David Wolinsky <davidiw@ufl.edu>
2010-12-27 13:26:13 -0500

    Fix a glibc double free exception

--- DRAMSim2/Rank.cpp
+++ DRAMSim2/Rank.cpp
@@ -266,7 +266,8 @@ void Rank::receiveFromBus(BusPacket *packet)
 #else
 		// end of the line for the write packet
 #endif
-		delete(packet);
+//    This causes a double free ... maybe there is a bug in the code somewhere
+//		delete(packet);
 		break;
 	default:
 		ERROR("== Error - Unknown BusPacketType trying to be sent to Bank");
